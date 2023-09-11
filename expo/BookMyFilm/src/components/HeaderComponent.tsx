93a4b6bde39f2d34c6b2f2d98c0f3f13a0b2f7a3
mateen27 <khanmateen2713@gmail.com>
2023-09-11 13:22:12 +0530

    Done with Splash/Notification/Header/Drawer Setup

---
+++ expo/BookMyFilm/src/components/HeaderComponent.tsx
@@ -0,0 +1,60 @@
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+// importing the bars image
+import { Entypo } from '@expo/vector-icons';
+import MenuBlack from '../../images/menu.png'
+import MenuWhite from '../images/menuWhite.png'
+// importing notification icon
+import { Ionicons } from '@expo/vector-icons';
+// responsive font imports
+import {
+    responsiveScreenHeight,
+    responsiveScreenWidth,
+    responsiveScreenFontSize
+  } from "react-native-responsive-dimensions";
+
+//   importing a screen
+// import Notification from '../screens/AppScreens/Notification';
+
+const HeaderComponent: React.FC<{navigation: any}> = ({navigation}) => {
+  return (
+    <SafeAreaView style = {styles.headerContainer}>
+        {/* Menu Bar Icon */}
+      <TouchableOpacity
+      onPress={() => navigation.openDrawer()}
+      ><Image 
+      source={MenuBlack}
+      style = {styles.headerMenu}
+      /></TouchableOpacity>
+
+      {/* Page Name Text */}
+      <Text style = {styles.headerText}>Home</Text>
+
+      {/* Notification Icon */} 
+      <TouchableOpacity
+      onPress={() => navigation.navigate('Notification')}
+      ><Ionicons name="notifications-outline" size={25} color="#0D0D0D" /></TouchableOpacity>
+    </SafeAreaView>
+  )
+}
+
+export default HeaderComponent
+
+const styles = StyleSheet.create({
+    headerContainer : {
+        marginVertical : '12%' , 
+        marginHorizontal : '5%' , 
+        flexDirection : 'row' , 
+        justifyContent : 'space-between' , 
+        alignItems : 'center'
+    } , 
+    headerText : {
+        fontSize : responsiveScreenFontSize(2.5) , 
+        fontWeight : "bold" , 
+        color : '#0D0D0D' , //#0D0D0D  //#f5f6fa //#CAD5E2
+    } , 
+    headerMenu : {
+        height: 22,
+        width: 32, 
+    }
+})
