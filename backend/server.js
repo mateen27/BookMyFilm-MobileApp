465019c3cc79a4604ec674cda6b03d3f9230041e
mateen27 <khanmateen2713@gmail.com>
2023-10-23 19:07:36 +0530

    Register User EndPoint API and Token Generation

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ const chats = require('./data/data');
 const connectDB = require('./config/database');
 const app = express();
 const colors = require('colors')
+const userRoutes = require('./routes/userRoutes')
 
 require('dotenv').config();
 
 // connecting to database
 connectDB();
+
+app.use(express.json()) // to accept JSON Data
 
 const PORT = process.env.PORT || 8080
 
@@ -15,17 +18,19 @@ app.get('/' , (req , res) => {
     res.send('API is running');
 })
 
-// endpoint for displaying all the chats
-app.get('/api/chat' , (req , res) => {
-    res.send(chats);
-})
+// // endpoint for displaying all the chats
+// app.get('/api/chat' , (req , res) => {
+//     res.send(chats);
+// })
 
-// endpoint for displaying particular chat by its id
-app.get('/api/chat/:id' , (req , res) => {
-    // console.log(req.params.id);
-    const singleChat = chats.find(c => c._id === req.params.id)
-    res.send(singleChat);
-})
+// // endpoint for displaying particular chat by its id
+// app.get('/api/chat/:id' , (req , res) => {
+//     // console.log(req.params.id);
+//     const singleChat = chats.find(c => c._id === req.params.id)
+//     res.send(singleChat);
+// })
+
+app.use('/api/user' , userRoutes)
 
 app.listen(PORT , () => {
     console.log(`server running at ${PORT}`.yellow.bold);
